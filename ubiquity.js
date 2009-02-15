d3840c0bf57098d6d8acea3d647ab0fb4bf7aa0b
Cosimo Streppone <cosimo@cpan.org>
2009-02-15 15:28:16 +0000

    Added lots of new commands
    git-svn-id: svn://localhost:81/ubiquity-opera@163 a22adaaf-26af-384a-bbed-527b507455b5

--- ubiquity.js
+++ ubiquity.js
@@ -6,6 +6,46 @@
 
 */
 
+var ubiq_commands = new Array (
+    'amazon-search',
+    'answers-search',
+    'ask-search',
+    'back',
+    'bugzilla',
+    'close',
+    'command-list',
+    'ebay-search',
+    'gcalculate',
+    'google',
+    'help',
+    'image-search',
+    'map',
+    //'opera-bugs',
+    'refresh',
+    'search',
+    'translate-no',
+    'weather'
+);
+var ubiq_commands_tip = new Array (
+    'Searches Amazon for books matching:',
+    'Searches Answers.com for:',
+    'Searches Ask.com for the given words',
+    'Go back 1 step in history',
+    'Perform a bugzilla search for',
+    'Close the current window',
+    'Shows the list of Ubiquity commands and what they do',
+    'Searches EBay for the given words',
+    'Examples: 3^4/sqrt(2)-pi,  3 inch in cm,  speed of light,  0xAF in decimal (<a href="http://www.googleguide.com/calculator.html">Command list</a>)',
+    'Searches Google for your words',
+    'Provides basic help on using Ubiquity for Opera',
+    'Search on Google Images',
+    'Shows a location on the map',
+    //'Search in the Opera bug tracking database for',
+    'Refreshes current document',
+    'Search using Google for:',
+    'Translates the given words from Norwegian to English',
+    'Shows the weather forecast for:'
+);
 var ubiq_window;
 var ubiq_selection;
 var ubiq_element;
@@ -48,7 +88,7 @@ function ubiq_start_mode () {
          + 'margin-left:10px; background:none; color:black;'
          + 'font-family: Trebuchet MS, Arial, Helvetica; font-size: 28px;';
     var div_style = 'width:100%; border:0; display:block; float:left; margin: 0px 5px 0px 5px;';
-	var results_panel_style = div_style + 'clear:both; text-align: left; padding-top: 8px; font-size: 1.4em; font-weight: 0; color:white; height: 502px;';
+	var results_panel_style = div_style + 'clear:both; text-align: left; padding-top: 8px; font-size: 19px; font-weight: normal; color:white; height: 502px;';
     var html =
           '<div id="ubiq-input-panel" style="' + div_style + 'height:55px">'
         + '<form id="ubiq1" onsubmit="return false">'
@@ -82,23 +122,63 @@ function ubiq_dispatch_command(line) {
         text= words.join(' ');
     }
 
-    if (cmd=='tran-no') {
+    if (cmd=='translate-no') { // TODO change this to generalize
         ubiq_cmd_translate_no(text);
     }
+    else if (cmd=='amazon-search') {
+        ubiq_cmd_url_based('http://www.amazon.com/s/ref=nb_ss_gw?url=search-alias%3Dstripbooks&field-keywords=' + escape(text));
+    }
+    else if (cmd=='answers-search') {
+        ubiq_cmd_url_based('http://www.answers.com/' + escape(text));
+    }
+    else if (cmd=='ask-search') {
+        ubiq_cmd_url_based('http://www.ask.com/web?q=' + escape(text));
+    }
+    else if (cmd=='back') {
+        history.go(-2);
+        ubiq_toggle_window(ubiq_window);
+    }
+    else if (cmd=='bugzilla') {
+        ubiq_cmd_url_based('https://bugzilla.mozilla.org/buglist.cgi?query_format=specific&order=relevance+desc&bug_status=__open__&content='+escape(text));
+    }
+    else if (cmd=='close') {
+        ubiq_toggle_window(ubiq_window);
+        window.close();
+    }
+    else if (cmd=='command-list') {
+        ubiq_show_matching_commands('*all');
+    }
+    else if (cmd=='ebay-search') {
+        ubiq_cmd_url_based('http://search.ebay.com/search/search.dll?satitle=' + escape(text));
+    }
+    else if (cmd=='image-search') {
+        ubiq_cmd_url_based('http://images.google.com/images?hl=en&q='+escape(text)+'&client=opera&sourceid=opera');
+    }
+    else if (cmd=='gcalculate') {
+        ubiq_cmd_url_based('http://www.google.com/search?client=opera&num=1&q='+escape(text)+'&sourceid=opera&ie=utf-8&oe=utf-8');
+    }
+    else if (cmd=='google' || cmd=='search') {
+        ubiq_cmd_url_based('http://www.google.com/search?client=opera&q='+escape(text)+'&sourceid=opera&ie=utf-8&oe=utf-8');
+    }
     else if (cmd=='map') {
-        ubiq_cmd_map(text);
+        ubiq_cmd_url_based('http://maps.google.com/maps?q='+escape(text));
     }
     else if (cmd=='refresh') {
         ubiq_cmd_refresh();
     }
     else if (cmd=='weather') {
-        ubiq_cmd_weather(text);
+        ubiq_cmd_url_based('http://www.wunderground.com/cgi-bin/findweather/getForecast?query='+escape(text));
     }
     else if (cmd=='h' || cmd=='help') {
         ubiq_display_results(ubiq_help());
     }
 
     return;
+}
+
+function ubiq_cmd_url_based (url) {
+    ubiq_toggle_window(ubiq_window);
+    ubiq_new_window(url);
 }
 
 function ubiq_display_results (text) {
@@ -126,16 +206,6 @@ function ubiq_cmd_translate_no(text) {
 function ubiq_cmd_refresh() {
     ubiq_toggle_window(ubiq_window);
     document.location.reload();
-}
-
-function ubiq_cmd_weather(text) {
-    ubiq_toggle_window(ubiq_window);
-    ubiq_new_window('http://www.wunderground.com/cgi-bin/findweather/getForecast?query='+escape(text));
-}
-
-function ubiq_cmd_map(address) {
-    ubiq_toggle_window(ubiq_window);
-    ubiq_new_window('http://maps.google.com/maps?q='+escape(address));
 }
 
 function ubiq_new_window(url) {
@@ -192,6 +262,45 @@ function ubiq_get_current_element () {
     return (ubiq_element = el);
 }
 
+function ubiq_show_matching_commands (text) {
+    if (! text) text = ubiq_command();
+    var show_all = text == '*all';
+    var matches = new Array();
+    if (text.length > 0) {
+        for (var c in ubiq_commands) {
+            c = ubiq_commands[c] + ' <span style="font-family:Helvetica,Arial;font-style:italic;font-size:14px;">' + ubiq_commands_tip[c] + '</span>';
+            if (show_all || c.match('^' + text)) {
+                matches.push(c);
+            }
+        }
+    }
+
+    // Where to show the results
+    var results_panel = document.getElementById('ubiq-results-panel');
+
+    // We have matches, show a list
+    if (matches.length > 0) {
+
+        var suggestions_div = document.createElement('div');
+        var suggestions_list = document.createElement('ul');
+
+        for (var c in matches) {
+            var li=document.createElement('li');
+            li.innerHTML=matches[c];
+            li.style = 'list-style: none; padding:0; margin:0; font-family: monospace; font-size: 1.0em';
+            suggestions_list.appendChild(li);
+        }
+
+        suggestions_div.appendChild(suggestions_list);
+        results_panel.innerHTML = suggestions_div.innerHTML;
+    }
+    else {
+        results_panel.innerHTML = ubiq_help();
+    }
+
+    return;
+}
+
 function ubiq_key_handler (userjs_event) {
     if (!userjs_event) return;
     var ev = userjs_event.event;
@@ -220,7 +329,12 @@ function ubiq_key_handler (userjs_event) {
 		}
 
         // On ENTER, execute the given command
-		if (kc==13) ubiq_execute();
+		if (kc==13) {
+            ubiq_execute();
+            return;
+        }
+
+        ubiq_show_matching_commands();
 	}
 
 }
