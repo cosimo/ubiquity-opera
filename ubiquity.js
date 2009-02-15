f71a43c51b030c18061e08a342dbb68ff85d1250
Cosimo Streppone <cosimo@cpan.org>
2009-02-15 13:44:33 +0000

    Added pre-selection on ubiquity window opening
    git-svn-id: svn://localhost:81/ubiquity-opera@160 a22adaaf-26af-384a-bbed-527b507455b5

--- ubiquity.js
+++ ubiquity.js
@@ -3,11 +3,23 @@
 	First version: Cosimo 17/01/2009
 
 	$Id$
+
 */
 
 var ubiq_window;
 var ubiq_selection;
 var ubiq_element;
+var ubiq_remote_server = 'http://people.opera.com/cosimo/ubiquity';
+
+// Used to get css url of images and other resources
+function ubiq_url_for (path) {
+    var url = 'url(';
+    url += ubiq_remote_server;
+    url += '/';
+    url += path;
+    url += ')';
+    return url;
+}
 
 function ubiq_create_window () {
     var doc = window.document;
@@ -24,7 +36,7 @@ function ubiq_create_window () {
     stl.padding='0';
     /* Our window should appear on top of everything */
     stl.zIndex='99999';
-    stl.background='url(http://www.streppone.it/ubiq/ubiq_background.png)';
+    stl.background = ubiq_url_for('ubiq_background.png');
     wnd.innerHTML = ubiq_start_mode();
     doc.body.appendChild(wnd);
     return wnd;
@@ -36,7 +48,7 @@ function ubiq_start_mode () {
          + 'margin-left:10px; background:none; color:black;'
          + 'font-family: Trebuchet MS, Arial, Helvetica; font-size: 28px;';
     var div_style = 'width:100%; border:0; display:block; float:left; margin: 0px 5px 0px 5px;';
-	var results_panel_style = div_style + 'clear:both; text-align: left; padding-top: 8px; font-size: 1.4em; color:white; height: 502px;';
+	var results_panel_style = div_style + 'clear:both; text-align: left; padding-top: 8px; font-size: 1.4em; font-weight: 0; color:white; height: 502px;';
     var html =
           '<div id="ubiq-input-panel" style="' + div_style + 'height:55px">'
         + '<form id="ubiq1" onsubmit="return false">'
@@ -59,80 +71,79 @@ function ubiq_execute () {
 }
 
 function ubiq_dispatch_command(line) {
-	var words = line.split(' ');
-	var cmd = words[0];
+    var words = line.split(' ');
+    var cmd = words[0];
 
-	var text;
-	if (ubiq_selection) {
-		text = ubiq_selection;
-	} else {
-		words[0] = '';
-		text= words.join(' ');
-	}
+    var text;
+    if (ubiq_selection) {
+        text = ubiq_selection;
+    } else {
+        words[0] = '';
+        text= words.join(' ');
+    }
 
-	if (cmd=='tran-no') {
-		ubiq_cmd_translate_no(text);
-	}
-	else if (cmd=='map') {
-		ubiq_cmd_map(text);
-	}
-	else if (cmd=='refresh') {
-		ubiq_cmd_refresh();
-	}
-	else if (cmd=='weather') {
-		ubiq_cmd_weather(text);
-	}
-	else if (cmd=='h' || cmd=='help') {
-		ubiq_display_results(ubiq_help());
-	}
+    if (cmd=='tran-no') {
+        ubiq_cmd_translate_no(text);
+    }
+    else if (cmd=='map') {
+        ubiq_cmd_map(text);
+    }
+    else if (cmd=='refresh') {
+        ubiq_cmd_refresh();
+    }
+    else if (cmd=='weather') {
+        ubiq_cmd_weather(text);
+    }
+    else if (cmd=='h' || cmd=='help') {
+        ubiq_display_results(ubiq_help());
+    }
 
-	return;
+    return;
 }
 
 function ubiq_display_results (text) {
-	//var wnd=ubiq_window;
-	var div=document.getElementById('ubiq-results-panel');
-	if (! div) alert('no div!');
-	opera.postError('help text='+text);
-	div.innerHTML = text;
-	div.style.visibility='show';
+    var div=document.getElementById('ubiq-results-panel');
+    if (! div) alert('no div!');
+    opera.postError('help text='+text);
+    div.innerHTML = text;
+    div.style.visibility='show';
 }
 
 function ubiq_help () {
-    var html = 'Type the name of a command and press enter to execute it, or <b>help</b> for assistance.';
-	return html;
+    var html = '<p>Type the name of a command and press enter to execute it, or <b>help</b> for assistance.</p>';
+    return html;
 }
 
 function ubiq_cmd_translate_no(text) {
-	ubiq_toggle_window(ubiq_window);
-	// HARD !!!
-	//alert(ubiq_element.innerHTML);
-	//var html = ubiq_element.innerHTML.replace(text, 'blah blah blah');
-	//ubiq_element.innerHTML = html;
-	ubiq_new_window('http://translate.google.com/translate_t?#no|en|'+text);
+    ubiq_toggle_window(ubiq_window);
+    // HARD !!!
+    //alert(ubiq_element.innerHTML);
+    //var html = ubiq_element.innerHTML.replace(text, 'blah blah blah');
+    //ubiq_element.innerHTML = html;
+    ubiq_new_window('http://translate.google.com/translate_t?#no|en|'+text);
 }
 
 function ubiq_cmd_refresh() {
-	ubiq_toggle_window(ubiq_window);
-	document.location.reload();
+    ubiq_toggle_window(ubiq_window);
+    document.location.reload();
 }
 
 function ubiq_cmd_weather(text) {
-	ubiq_toggle_window(ubiq_window);
-	ubiq_new_window('http://www.wunderground.com/cgi-bin/findweather/getForecast?query='+escape(text));
+    ubiq_toggle_window(ubiq_window);
+    ubiq_new_window('http://www.wunderground.com/cgi-bin/findweather/getForecast?query='+escape(text));
 }
 
 function ubiq_cmd_map(address) {
-	ubiq_toggle_window(ubiq_window);
-	ubiq_new_window('http://maps.google.com/maps?q='+escape(address));
+    ubiq_toggle_window(ubiq_window);
+    ubiq_new_window('http://maps.google.com/maps?q='+escape(address));
 }
 
 function ubiq_new_window(url) {
-	window.open(url, 'ubiq_tab');
+    window.open(url, 'ubiq_tab');
 }
 
 function ubiq_get_selection () {
-	var str = '';
+    var str = '';
     if (document.getSelection) {
     	str = document.getSelection();
     } else if (document.selection && document.selection.createRange) {
@@ -152,6 +163,8 @@ function ubiq_toggle_window (w) {
 
 function ubiq_focus (w) {
     var line=document.getElementById('ubiq_input');
+    // Pre-select the input content if there's any
+    if (line.value != '') line.createTextRange().select();
     line.focus();
 }
 
@@ -171,59 +184,49 @@ function ubiq_command () {
 
 // Gets current selection element
 function ubiq_get_current_element () {
-	var el;
-	if (document.selection && document.selection.createRange) {
-		var range = document.selection.createRange();
-		el = range.parentElement();
-	}
-	return (ubiq_element = el);
+    var el;
+    if (document.selection && document.selection.createRange) {
+        var range = document.selection.createRange();
+        el = range.parentElement();
+    }
+    return (ubiq_element = el);
 }
 
 function ubiq_key_handler (userjs_event) {
     if (!userjs_event) return;
     var ev = userjs_event.event;
 	var kc = ev.keyCode;
-	var ctrl_space_pressed = ev.ctrlKey && kc==32;
+	var ctrl_space_pressed = kc == 32 && ev.ctrlKey;
 
     // If we're in the background (or not created), return immediately
     // Otherwise, activate only on CTRL + Space
 	if (! ubiq_enabled()) {
-
-		// Create our window if not already done 
+    	// Create our window if not already done 
 		if (! ubiq_window)
 			ubiq_window = ubiq_create_window();
 
 		if (ctrl_space_pressed) {
-
     		// Get text selection before switching window focus
 			ubiq_get_selection();
-
-    		// Get text selection before switching window focus
 			ubiq_get_current_element();
-
-    		// Get text selection before switching window focus
 			ubiq_toggle_window(ubiq_window);
-
-    		// And focus on the input line
 			ubiq_focus(ubiq_window);
 		}
 	}
-
 	else {
-
 		if (ctrl_space_pressed) {
 			ubiq_toggle_window(ubiq_window);
 			return;
 		}
 
-		if (kc==13) {
-			ubiq_execute();
-		}
-
+        // On ENTER, execute the given command
+		if (kc==13) ubiq_execute();
 	}
 
 }
 
 /* Add event handler to window */
 window.opera.addEventListener('afterEvent.keyup', ubiq_key_handler, false);
+
+// vim: ts=4 sw=4 tw=0 et
 
